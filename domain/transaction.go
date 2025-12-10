835797e320eba2ed0043c2c82122842936a12c46
brandaoplaster <brandaoplaster@gmail.com>
2025-12-09 21:02:51 -0300

    feat: Add functions newTransactiona and processAndValidate

--- domain/transaction.go
+++ domain/transaction.go
@@ -1,6 +1,16 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type TransactionRepository interface {
+	SaveTransaction(transaction Transaction, creditCard CreditCard) error
+	GetCreditCard(creditCard CreditCard) (CreditCard, error)
+	CreateCreditCard(creditCard CreditCard) error
+}
 
 type Transaction struct {
 	ID           string
@@ -10,4 +20,20 @@ type Transaction struct {
 	Store        string
 	CreditCardId string
 	CreatedAt    time.Time
+}
+
+func NewTransaction() *Transaction {
+	transaction := &Transaction{}
+	transaction.ID = uuid.NewV4().String()
+	transaction.CreatedAt = time.Now()
+	return transaction
+}
+
+func (transaction *Transaction) ProcessAndValidate(creditCard *CreditCard) {
+	if transaction.Amount+creditCard.Balance > creditCard.Limit {
+		transaction.Status = "rejected"
+	} else {
+		transaction.Status = "approved"
+		creditCard.Balance = creditCard.Balance + transaction.Amount
+	}
 }
