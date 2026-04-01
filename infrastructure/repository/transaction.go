ceecfcd4285e009055a09eee3a96df9c0ec28d73
brandaoplaster <brandaoplaster@gmail.com>
2026-04-01 10:41:41 -0300

    chore: Adjust code

--- infrastructure/repository/transaction.go
+++ infrastructure/repository/transaction.go
@@ -1,49 +1,51 @@
+// Package repository handles data persistence for the application.
 package repository
 
 import (
-    "database/sql"
-    "github.com/brandaoplaster/payment-gateway/domain"
+	"database/sql"
+
+	"github.com/brandaoplaster/payment-gateway/domain"
 )
 
 type TransactionRepository struct {
-    db               *sql.DB
-    creditCardRepo   *CreditCardRepository
+	db             *sql.DB
+	creditCardRepo *CreditCardRepositoryDb
 }
 
-func NewTransactionRepository(db *sql.DB, ccRepo *CreditCardRepository) *TransactionRepository {
-    return &TransactionRepository{
-        db:             db,
-        creditCardRepo: ccRepo,
-    }
+func NewTransactionRepository(db *sql.DB, ccRepo *CreditCardRepositoryDb) *TransactionRepository {
+	return &TransactionRepository{
+		db:             db,
+		creditCardRepo: ccRepo,
+	}
 }
 
 func (r *TransactionRepository) SaveTransaction(transaction domain.Transaction, creditCard domain.CreditCard) error {
-    stmt, err := r.db.Prepare(`insert into transactions(id, credit_card_id, amount, status, description, store, created_at)
+	stmt, err := r.db.Prepare(`insert into transactions(id, credit_card_id, amount, status, description, store, created_at)
                                 values($1, $2, $3, $4, $5, $6, $7)`)
-    if err != nil {
-        return err
-    }
-    defer stmt.Close()
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
-    _, err = stmt.Exec(
-        transaction.ID,
-        transaction.CreditCardId,
-        transaction.Amount,
-        transaction.Status,
-        transaction.Description,
-        transaction.Store,
-        transaction.CreatedAt,
-    )
-    if err != nil {
-        return err
-    }
+	_, err = stmt.Exec(
+		transaction.ID,
+		transaction.CreditCardId,
+		transaction.Amount,
+		transaction.Status,
+		transaction.Description,
+		transaction.Store,
+		transaction.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
 
-    if transaction.Status == "approved" {
-        err = r.creditCardRepo.UpdateBalance(creditCard)
-        if err != nil {
-            return err
-        }
-    }
+	if transaction.Status == "approved" {
+		err = r.creditCardRepo.updateBalance(creditCard)
+		if err != nil {
+			return err
+		}
+	}
 
-    return nil
+	return nil
 }
